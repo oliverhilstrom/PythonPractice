2e7a6d243e63839fc56853f602e1ed7bffd7624f
oliverhilstrom <819492@mystma.org>
2021-09-13 15:07:55 -0500

    added movies and a short dialouge

---
+++ main.py
@@ -0,0 +1,14 @@
+print("Hello my world.")
+print("It's time we should talk.")
+print("Me and your mother are seperating.")
+print("I am in love with the Weatherman.")
+#it's not funny
+
+#Print title
+print("Favorite Movies")
+#Print first movie
+print("Scott Pilgrim")
+#Print second movie
+print("Cabin in the Woods")
+#Print third movie but fail
+print("uh brain no think")
