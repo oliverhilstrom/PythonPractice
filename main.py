3d5da65a322c32497bf0b13b5b7eb9946fa5debd
oliverhilstrom <819492@mystma.org>
2021-09-14 15:14:20 -0500

    define command practice

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-print("Hello my world.")
-print("It's time we should talk.")
-print("Me and your mother are seperating.")
-print("I am in love with the Weatherman.")
-#it's not funny
+#define movies
+def topFiveMovies():
+    #print title
+    print("Top Five Movies:")
+    #print one movie
+    print("-Scott Pilgrim vs The World")
+    #print two movie
+    print("-Cabin in the Woods")
+    #print three movie
+    print("-Scooby Doo")
+    #print four movie
+    print("-Scooby Doo 2")
+    #print five movie
+    print("-one of those dog ones")
+topFiveMovies()
 
-#Print title
-print("Favorite Movies")
-#Print first movie
-print("Scott Pilgrim")
-#Print second movie
-print("Cabin in the Woods")
-#Print third movie but fail
-print("uh brain no think")
+
+#define dog
+def dogMovies():
+    #print title
+    print("Some of the Dog ones:")
+    print("-A Dogs Purpose")
+    print("-Marly and Me")
+    print("-Back at the Barnyard")
+dogMovies()
