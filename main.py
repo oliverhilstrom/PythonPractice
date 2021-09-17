6d679ed7423f21d6f22e81dee032cb499dff3a25
oliverhilstrom <819492@mystma.org>
2021-09-17 14:19:12 -0500

    fixed spaces in madlibs

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ def addTwoNumbers(one, two):
 addTwoNumbers(74, 23)
 
 def madLibs(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r):
-    print("While I've stayed at home, I've practiced the" + a + "for" + b + "hours every" + c + "." +
-    "My favorite key signature to play and practice in is" + d + e + "." + "It has" + f + "flats and" + g + "sharps. I like this key signature because it is" + h +
-    "and" + i + ". I've also gotten awesome at counting time signatures. The time signature I love is" + j + ", because it is" + k + "to count. One insturment I want to learn how to play while I am at home is"
-    + l + ", because it is" + m + "and sounds like a" + n + ". My" + o + "likes it when I play" + p + ", and always gives me a round of applause after my performances! When I return to school, my teacher will be" + q + "of how great I am at playing my insurment. My teacher might even think I sound like" + r + "!")
-madLibs("stylophone", "seventeen", "sunday", "z", "minor", "six", "seven", "juicy", "puffy", "four-fourths", "exuberant", "tambourine", "spicy", "tiny cymbal", "dead grandpa", "Crazy Frog", "proud", "Lemon Demon")
+    print("While I've stayed at home, I've practiced the " + a + " for " + b + " hours every " + c + "." +
+    "My favorite key signature to play and practice in is " + d + e + ". " + "It has " + f + " flats and " + g + " sharps. I like this key signature because it is " + h +
+    " and" + i + ". I've also gotten awesome at counting time signatures. The time signature I love is" + j + ", because it is " + k + " to count. One insturment I want to learn how to play while I am at home is"
+    + l + ", because it is " + m + " and sounds like a " + n + ". My " + o + " likes it when I play " + p + ", and always gives me a round of applause after my performances! When I return to school, my teacher will be " + q + " of how great I am at playing my insurment. My teacher might even think I sound like " + r + "!")
+madLibs("stylophone", "seventeen", "sunday", "z", " minor", "six", "seven", "juicy", " slimy", " four-fourths", "exuberant", " tambourine", "spicy", "tiny cymbal", "dead grandpa", "Crazy Frog", "proud", "Lemon Demon")
