6069b64c33bf5e8c9351c9c7a80fa9d4ca2e1ab7
oliverhilstrom <819492@gmail.com>
2021-09-24 15:14:17 -0500

    if else and elif

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
-def doingMaths(number):
-    print( number + 5 / 2)
-doingMaths(4) 
+def greaterThan10(x):
+    if x > 10:
+        return "x is greater than 10"
 
-def plusFive(x):
-    print(x + 5)
-plusFive(72)
+    elif x == 10:
+        return "x is equal to 10"
 
-def dividedTwo(y):
-    print(y / 2)
-dividedTwo(89)
+    else:
+         return "x is not greater than 10"
+print(greaterThan10(10))
 
-def addTwoNumbers(one, two):
-    print(one + two)
-addTwoNumbers(74, 23)
 
-def madLibs(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r):
-    print("While I've stayed at home, I've practiced the " + a + " for " + b + " hours every " + c + "." +
-    "My favorite key signature to play and practice in is " + d + e + ". " + "It has " + f + " flats and " + g + " sharps. I like this key signature because it is " + h +
-    " and" + i + ". I've also gotten awesome at counting time signatures. The time signature I love is" + j + ", because it is " + k + " to count. One insturment I want to learn how to play while I am at home is"
-    + l + ", because it is " + m + " and sounds like a " + n + ". My " + o + " likes it when I play " + p + ", and always gives me a round of applause after my performances! When I return to school, my teacher will be " + q + " of how great I am at playing my insurment. My teacher might even think I sound like " + r + "!")
-madLibs("stylophone", "seventeen", "sunday", "z", " minor", "six", "seven", "juicy", " slimy", " four-fourths", "exuberant", " tambourine", "spicy", "tiny cymbal", "dead grandpa", "Crazy Frog", "proud", "Lemon Demon")
+
+def potato(x, y):
+    if (x + y) > 10:
+        return "potato"
+
+    elif (x + y) == 10:
+        return "tomato"
+
+    else:
+        return "alfredo"
+print(potato(8, 4))
