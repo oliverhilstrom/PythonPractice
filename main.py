255747f2f49c5fa91a2ee64ef7b6cda0dd0d28b1
oliverhilstrom <819492@gmail.com>
2021-09-30 15:16:47 -0500

    whats going on

--- main.py
+++ main.py
@@ -1,47 +1,9 @@
-def define(word):
-    rat = rat.lower()
-    rat = rat.upper()
-    if word == "rat":
-        return "Small dog"
-    pizzaball = pizzaball.lower()
-    pizzaball = pizzaball.upper()
-    elif word == "pizzaball":
-        return "Pizza in ball form."
-    garfield = garfield.lower()
-    garfield = garfield.upper()
-    elif word == "garfield":
-        return "An orange cat in a well-known comic strip. He is also featured in two of his own movies and an animated show"
-    oven = oven.lower()
-    oven = oven.upper()
-    elif word == "Oven":
-        return "why do they call it oven when you of in the cold food or out the hot eat the food
-    dog = dog.lower()
-    dog = dog.upper()
-    elif word == "Dog":
-        return "big rat"
-    Osowski = Osowski.lower()
-    Osowski = Osowski.upper()
-    elif word == "Osowski":
-        return "cool lad. could be considered the sixth mothersauce of france"
-    beans = beans.lower()
-    beans = beans.upper()
-    elif word == "beans":
-        return "jacobs choice of beverage"
-    chair = chair.lower()
-    chair = chair.upper()
-    elif word == "chair":
-        return "i sit."
-    meterstick = meterstick.lower()
-    meterstick = meterstick.upper()
-    elif word == "meterstick":
-        return "about a meter long stick with marks on it."
-    water = water.lower()
-    water = water.upper()
-    elif word == "water":
-        return "the drink"
-    pen = pen.lower()
-    pen = pen. upper()
-    elif word == "pen":
-        return "the ink"
-    else:
-        return "This word does not exist"
+def today():
+    x = 1
+    while x <= 20:
+        if x % 2 == 0:
+            print(x)
+            print("even")
+        else:
+            print(x)
+            print("odd")
